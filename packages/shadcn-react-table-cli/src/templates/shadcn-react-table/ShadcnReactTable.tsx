1e1ebb6a9bc61a7e9e1157ee58820538e808b0c1
anindo-mac <sarkeranindo@gmail.com>
2025-09-24 03:26:05 +0600

    update cli

--- packages/shadcn-react-table-cli/src/templates/shadcn-react-table/ShadcnReactTable.tsx
+++ packages/shadcn-react-table-cli/src/templates/shadcn-react-table/ShadcnReactTable.tsx
@@ -1,41 +1,39 @@
-import { useDataTable, type ColumnDef } from 'shadcn-react-table-core';
-import { Header } from './head/Header';
+import {
+  useShadcnReactTable,
+  type SRT_RowData,
+  type SRT_TableInstance,
+  type SRT_TableOptions,
+  type Xor,
+} from 'shadcn-react-table-core';
+import SRT_TableLayout from './table/SRT_TableLayout';
 
-export type ShadcnReactTableProps<TData> = {
-  columns: ColumnDef<TData, any>[];
-  data: TData[];
-  className?: string;
+type TableInstanceProp<TData extends SRT_RowData> = {
+  table: SRT_TableInstance<TData>;
 };
 
-export function ShadcnReactTable<TData>({
-  columns,
-  data,
-  className,
-}: ShadcnReactTableProps<TData>) {
-  const table = useDataTable({ columns, data });
-  return (
-    <div className={className ?? 'w-full overflow-x-auto'}>
-      <table className="w-full text-sm">
-        <Header table={table} />
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr
-              key={row.id}
-              className="border-b transition-colors hover:bg-gray-50"
-            >
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} className="p-2 align-middle">
-                  {cell.column.columnDef.cell
-                    ? (cell.column.columnDef.cell as any)({
-                        ...cell.getContext(),
-                      })
-                    : (cell.getValue() as any)}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
+export type ShadcnReactTableProps<TData extends SRT_RowData> = Xor<
+  TableInstanceProp<TData>,
+  SRT_TableOptions<TData>
+>;
+
+const isTableInstanceProp = <TData extends SRT_RowData>(
+  props: ShadcnReactTableProps<TData>,
+): props is TableInstanceProp<TData> =>
+  (props as TableInstanceProp<TData>).table !== undefined;
+
+export const ShadcnReactTable = <TData extends SRT_RowData>(
+  props: ShadcnReactTableProps<TData>,
+) => {
+  let table: SRT_TableInstance<TData>;
+
+  if (isTableInstanceProp(props)) {
+    table = props.table;
+  } else {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    table = useShadcnReactTable(props);
+  }
+
+  return <SRT_TableLayout table={table} />;
+};
+
+export default ShadcnReactTable;
