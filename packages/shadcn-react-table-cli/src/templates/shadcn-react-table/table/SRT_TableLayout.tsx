0afbe9c27ce582d47b5e6fc30599ef133d59c92a
anindo-mac <sarkeranindo@gmail.com>
2025-09-24 07:14:36 +0600

    update cli

--- packages/shadcn-react-table-cli/src/templates/shadcn-react-table/table/SRT_TableLayout.tsx
+++ packages/shadcn-react-table-cli/src/templates/shadcn-react-table/table/SRT_TableLayout.tsx
@@ -1,15 +1,64 @@
-import type { SRT_RowData, SRT_TableInstance } from 'shadcn-react-table-core';
+import {
+  parseFromValuesOrFunc,
+  type LayoutDivProps,
+  type SRT_RowData,
+  type SRT_TableInstance,
+} from 'shadcn-react-table-core';
+import { cva } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
 import SRT_TableContainer from './SRT_TableContainer';
 
-export interface SRT_TableLayoutProps<TData extends SRT_RowData> {
+export interface SRT_TableLayoutProps<TData extends SRT_RowData>
+  extends LayoutDivProps {
   table: SRT_TableInstance<TData>;
 }
 
+const tableLayoutVariants = cva(
+  'relative overflow-hidden rounded-md border bg-background shadow transition-all duration-100 p-2',
+  {
+    variants: {
+      fullscreen: {
+        true: 'fixed inset-0 z-50 h-dvh w-screen rounded-none border-0 m-0',
+        false: '',
+      },
+    },
+    defaultVariants: {
+      fullscreen: false,
+    },
+  },
+);
+
 export const SRT_TableLayout = <TData extends SRT_RowData>({
   table,
+  ...rest
 }: SRT_TableLayoutProps<TData>) => {
+  const {
+    getState,
+    options,
+    refs: { tableLayoutRef },
+  } = table;
+
+  const { isFullScreen } = getState();
+
+  const layoutDivProps = {
+    ...parseFromValuesOrFunc(options?.srtTableLayoutProps, { table }),
+    ...rest,
+  };
+  const { className, ...divRest } = layoutDivProps;
+
+  // TODO: I've omitted the ref since this code will live in user's directory, might add later
+
   return (
-    <div>
+    <div
+      ref={(ref: HTMLDivElement) => {
+        tableLayoutRef.current = ref;
+      }}
+      onKeyDown={(e) => e.key === 'Escape' && table.setIsFullScreen(false)}
+      className={cn(
+        tableLayoutVariants({ fullscreen: isFullScreen, className }),
+      )}
+      {...divRest}
+    >
       <SRT_TableContainer table={table} />
     </div>
   );
